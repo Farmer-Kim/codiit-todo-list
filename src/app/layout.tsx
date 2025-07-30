491c0cfcde57e1eb5a7bd3f80c2dc02ed09b31e6
agent_kim <daegeon.developer@gmail.com>
2025-07-30 17:24:25 +0900

    feat: CSS 스타일 기본 세팅 및 이미지 에셋 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./reset.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+       {/* NanumSquare CDN */}
+       <link
+          href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css"
+          rel="stylesheet"
+        />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
